dab24a69afffb6deecbee9696e6b5a55848a241b
obashir <osama.bashir@zalando.de>
2021-02-06 20:07:21 +0100

    files refactoring

---
+++ Leetcode.playground/Sources/LinkListNode.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
--- Leetcode.playground/Sources/TreeNode.swift
+++ Leetcode.playground/Sources/TreeNode.swift
@@ -1,13 +1,5 @@
 import Foundation
 
-public class ListNode {
-    public var val: Int
-    public var next: ListNode?
-    public init(_ val: Int) {
-        self.val = val
-        self.next = nil
-    }
-}
 
 public class TreeNode {
     public var val: Int
