4dc5c7690e116cc559933c4446cf10206cce8bf7
Osama Bin Bashir <osama.binbashir@yahoo.com>
2021-03-20 12:39:18 +0100

    Update 0-1knapsack.swift

--- DP/0-1KnapSack/0-1knapsack.swift
+++ DP/0-1KnapSack/0-1knapsack.swift
@@ -1,28 +1,17 @@
 /// https://www.educative.io/courses/grokking-dynamic-programming-patterns-for-coding-interviews/RM1BDv71V60
-func solveKnapSack(_ costs: [Int], _ profits: [Int], _ maxCost: Int) -> Int {
-    var dp = Array(repeating: Array(repeating: -1, count: maxCost + 1), count: profits.count)
-    return knapSackRecursive(&dp, maxCost: maxCost, index: 0, cost: costs, profits: profits)
+func knapsack(_ wt:[Int], _ val: [Int], _ totalWt: Int) -> Int{
+    var cache = Array(repeating: Array(repeating: -1, count: totalWt + 1), count: wt.count + 1)
+    return knapsackRecursive(wt,  val, totalWt, &cache, index: 0)
 }
 
-
-func knapSackRecursive( _ dp: inout [[Int]], maxCost: Int, index: Int, cost: [Int], profits: [Int]) -> Int {
-
-    if index < 0 || index > profits.count - 1 { return 0 }
-
-    if dp[index][maxCost] != -1 { return dp[index][maxCost] }
-
-    var value1 = 0
-
-    if cost[index] <= maxCost {
-        value1 = profits[index] + knapSackRecursive(&dp, maxCost: maxCost - cost[index], index: index + 1, cost: cost, profits: profits)
-    }
-
-    let value2 = knapSackRecursive(&dp, maxCost: maxCost, index: index + 1, cost: cost, profits: profits)
-
-    dp[index][maxCost] = max(value1, value2)
-    return dp[index][maxCost]
-
+func knapsackRecursive(_ wt:[Int], _ val: [Int], _ totalWt: Int, _ cache: inout [[Int]], index: Int) -> Int {
+    if totalWt <= 0 || index > wt.count - 1 { return 0 }
+    if cache[index][totalWt] != -1 { return cache[index][totalWt] }
+    var profit1 = 0
+    if totalWt >= wt[index] { profit1 = val[index] + knapsackRecursive(wt, val, totalWt - wt[index], &cache, index: index + 1) }
+    let profit2 = knapsackRecursive(wt, val, totalWt, &cache, index: index + 1)
+    cache[index][totalWt] = max(profit1, profit2)
+    return cache[index][totalWt]
 }
 
-
-solveKnapSack([1, 2, 3, 5], [1, 6, 10, 16], 7)
+knapsack([1, 2, 3, 5], [1, 6, 10, 16], 7)
