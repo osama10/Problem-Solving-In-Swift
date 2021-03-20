cd2798ed2e46c687a65c959f61eea88a962d4eae
Osama Bin Bashir <osama.binbashir@yahoo.com>
2021-03-20 17:04:25 +0100

    Update LIS.swift

--- DP/LIS/LIS.swift
+++ DP/LIS/LIS.swift
@@ -2,15 +2,21 @@
 
 func longestIncreasingSubsequence(_ arr: [Int]) -> Int {
     var lis = Array(repeating: 1, count: arr.count)
-
+     var subseq = Array(repeating: [Int](), count: arr.count)
+     subseq[0] = [arr[0]]
+    
     for i in 0..<arr.count {
         for j in 0..<i {
             if arr[i] > arr[j] && lis[i] < (lis[j] + 1) {
                 lis[i] = lis[j] + 1
+                if i != 0 { subseq[i] = subseq[j] }
             }
         }
+        if i != 0 { subseq[i].append(arr[i]) }
     }
-
+    
+    print( subseq.filter{ (lis.max() ?? 1 == $0.count) } )
+    
     return lis.max() ?? 1
 }
 
